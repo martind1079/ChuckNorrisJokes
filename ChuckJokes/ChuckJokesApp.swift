3218721501bc15c4ff1f79e6202b20606f33140c
Martin Doyle <martin.doyle@thinkproject.com>
2022-03-13 18:18:28 +0000

    Initial Commit

---
+++ ChuckJokes/ChuckJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuckJokesApp.swift
+//  ChuckJokes
+//
+//  Created by Martin Doyle on 13/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChuckJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
